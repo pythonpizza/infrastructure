3364cd47206a48ad28c377b6848117f53746114b
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-10 01:58:34 +0100

    Give access to s3 to CI

--- global/ci_iam.tf
+++ global/ci_iam.tf
@@ -6,3 +6,24 @@ resource "aws_iam_user" "ci" {
 resource "aws_iam_access_key" "key" {
   user = aws_iam_user.ci.name
 }
+
+resource "aws_iam_user_policy" "s3_access" {
+  name   = "s3access"
+  user   = aws_iam_user.ci.name
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    sid    = "S3CIAccess"
+    effect = "Allow"
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      "arn:aws:s3:::*.python.pizza/*"
+    ]
+  }
+}
