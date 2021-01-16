79117f83a1ec043e4ef5f94ddb886b8320841e84
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-16 03:30:03 +0100

    Fix path

--- script/request-deploy/request-deploy-setup.sh
+++ script/request-deploy/request-deploy-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip install httpx
 
-wget https://raw.githubusercontent.com/pythonpizza/infrastructure/script/request-deploy/request-deploy.py
+wget https://raw.githubusercontent.com/pythonpizza/infrastructure/main/script/request-deploy/request-deploy.py
 
 python request-deploy.py
