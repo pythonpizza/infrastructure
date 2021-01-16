cb81c03da0c5b4ca3ad2909c2ea2c0702d17c554
Marco Acierno <marcoaciernoemail@gmail.com>
2021-01-16 16:07:53 +0100

    Fix file name

--- script/delete-environment/delete-environment-setup.sh
+++ script/delete-environment/delete-environment-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip install httpx
 
-wget https://raw.githubusercontent.com/pythonpizza/infrastructure/main/script/request-deploy/request-deploy.py
+wget https://raw.githubusercontent.com/pythonpizza/infrastructure/main/script/delete-environment/delete-environment.py
 
 python delete-environment.py
